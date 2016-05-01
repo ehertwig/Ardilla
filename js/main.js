e317e3dc07d3a1c1ebed804dbdc997e166b4cf5c
ehertwig <e3222@hotmail.com>
2016-05-01 18:02:57 -0500

    light-boxes

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 $(document).ready(function(){
-    $('.form-button').click(function(){
-      if($('.form-name').match(/[0-9]+/)) {
-        $('.alert').css('display','block');
-      }
+    
+    $('.image-box-maddy').click(function(){
+       $('.maddy-lightbox, .backdrop').css('display','block');
+        $('.maddy-lightbox').animate({'opacity':'1.0'}, 300, 'linear');
+       $('.backdrop').css('opacity', '.75');
     });
+    
+    $('.backdrop').click(function(){
+        closeBox();
+    });
+    
+    $('.close').click(function(){
+        closeBox();
+    });
+    
+    function closeBox(){
+        $('.maddy-lightbox, .backdrop').animate({'opacity': '0'}, 300, 'linear', function(){
+          $('.maddy-lightbox, .backdrop').css('display','none');
+         });
+    }
+    
+    $('.image-box-violeta').click(function(){
+        $('.violeta-lightbox, .backdrop').css('display','block');
+        $('.violeta-lightbox').animate({'opacity':'1.0'}, 300, 'linear');
+       $('.backdrop').css('opacity', '.75');
+    });
+    
+    
+    function closeBox(){
+        $('.violeta-lightbox, .backdrop').animate({'opacity': '0'}, 300, 'linear', function(){
+          $('.violeta-lightbox, .backdrop').css('display','none');
+         });
+    }
 });
